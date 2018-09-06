bda6420f38db69955c7d2787451b60fbef1c7c59
Kevin Haslett <kah_272@hotmail.com>
2018-09-06 00:57:27 -0400

    Remove reflect.Select because it's shit in this situation We still have 1 channel per pixel though which isn't a good idea

--- src/main.go
+++ src/main.go
@@ -7,7 +7,6 @@ import (
 	"image"
 	"image/png"
 	"math/rand"
-	"reflect"
 )
 
 func check(e error) {
@@ -20,12 +19,11 @@ func main() {
 	f, err := os.OpenFile("output/image.png", syscall.O_WRONLY | syscall.O_CREAT, 0777)
 	check(err)
 
-	width := 20
-	height := 20
+	width := 500
+	height := 500
 	numPixels := width * height
 
 	chans := make([]chan color.NRGBA, numPixels)
-	cases := make([]reflect.SelectCase, numPixels)
 
 	image := image.NewNRGBA(image.Rect(0, 0, width, height))
 	for x := 0; x < width; x++ {
@@ -33,22 +31,13 @@ func main() {
 			ch := pixel(x, y)
 			i := x * height + y
 			chans[i] = ch
-			cases[i] = reflect.SelectCase{
-				Dir: reflect.SelectRecv,
-				Chan: reflect.ValueOf(ch),
-			}
 		}
 	}
 
 	for i := 0; i < numPixels; i++ {
-		// reflect.Select is really bad here
-		// It's super slow, and it breaks with more than 65536 channels
-		// Currently we have 1 channel per pixel (which is probably a bad idea)
-		// And we definitely want to have more than 256 * 256 pixel images
-		chosen, value, _ := reflect.Select(cases)
-		x := chosen / height
-		y := chosen % height
-		colour := value.Interface().(color.NRGBA)
+		x := i / height
+		y := i % height
+		colour := <- chans[i]
 		image.Set(x, y, colour)
 	}
 
